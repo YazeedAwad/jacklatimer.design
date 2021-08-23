a49a2e311b7350963da148d4840e85c8428b3206
Jack Latimer <jacklatimer03@gmail.com>
2021-08-23 20:30:16 +0100

    jacklatimer.design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     maxWidth: {
-      '10xl': '90%',
-      '11xl': '92%',
-     }
+      "10xl": "90%",
+      "11xl": "92%",
+    },
   },
   variants: {
     extend: {},
