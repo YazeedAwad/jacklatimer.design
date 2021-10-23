d51162cffc8e9cfeb8d27256b1bcd00e5dea7ff2
Jack Latimer <jacklatimer03@gmail.com>
2021-10-23 15:10:52 +0100

    done(next/image): configured next/image

--- components/ui/Main.tsx
+++ components/ui/Main.tsx
@@ -1,6 +1,6 @@
+import Image from "next/image";
 import { motion } from "framer-motion";
 
-/* eslint-disable @next/next/no-img-element */
 export default function Main() {
   return (
     <div className="container mx-auto max-w-10xl pt-20">
@@ -26,89 +26,76 @@ export default function Main() {
       >
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 ">
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full  mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
+            <div className="xl:w-1/3 md:w-1/2 p-4 mb-6">
+              <Image
                 src="https://jacklatimer.design/images/placeholder.png"
                 alt="Design"
-              />
-            </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
-                src="https://jacklatimer.design/images/placeholder.png"
-                alt="Design"
-              />
-            </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
-                src="https://jacklatimer.design/images/placeholder.png"
-                alt="Design"
-              />
-            </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <img
-                className="w-full object-cover object-center mb-6"
-                src="https://jacklatimer.design/images/placeholder.png"
-                alt="Design"
+                height={1100}
+                width={1100}
               />
             </div>
           </div>
